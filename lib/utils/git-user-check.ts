c3f82d27ab4cc749fe22fdfa79f535d6b813226d
Eric Vandenberg <contact@ericvandenberg.com>
2024-04-04 03:26:42 -0600

    fix: rework git user check for missing files

--- lib/utils/git-user-check.ts
+++ lib/utils/git-user-check.ts
@@ -3,34 +3,79 @@ import { accessSync, readFileSync, constants } from 'node:fs'
 
 export const git_user_check = async (path: string) => {
   const home = os.homedir();
-  accessSync(`${home}/.gitconfig`, constants.R_OK | constants.W_OK);
+  const global_path = `${home}/.gitconfig`;
+  const local_path = `${path}/.git/config`;
+  let skip_global = false;
+  let skip_local = false;
+  const result: {
+    global: {
+      email?: string,
+      user?: string,
+    },
+    local: {
+      email?: string,
+      user?: string,
+    }
+  } = {
+    global: {
+      email: undefined,
+      user: undefined,
+    },
+    local: {
+      email: undefined,
+      user: undefined,
+    }
+  }
+
+  try {
+    accessSync(global_path, constants.R_OK | constants.W_OK);
+  } catch (err: unknown) {
+    const message = (err as Error).message;
+
+    if (message.includes(global_path)) {
+      skip_global = true;
+    }
+  }
+
+  try {
+    accessSync(local_path, constants.R_OK | constants.W_OK);
+  } catch (err: unknown) {
+    const message = (err as Error).message;
+
+    if (message.includes(local_path)) {
+      skip_local = true;
+    }
+  }
+
 
   const eregex = /email = (.*?)$/sm
   const uregex = /name = (.*?)$/sm
 
-  const gconfig = readFileSync(`${home}/.gitconfig`, 'utf-8');
-  const lconfig = readFileSync(`${path}/.git/config`, 'utf-8');
+  if (!skip_global) {
+    const gconfig = readFileSync(global_path, 'utf-8');
+    const gemail_match = gconfig.match(eregex);
+    const guser_match = gconfig.match(uregex);
+    const gemail = gemail_match ? gemail_match[1] : undefined;
+    const guser = guser_match ? guser_match[1] : undefined;
 
-  const gemail_match = gconfig.match(eregex);
-  const guser_match = gconfig.match(uregex);
-
-  const gemail = gemail_match ? gemail_match[1] : undefined;
-  const guser = guser_match ? guser_match[1] : undefined;
-
-  const lemail_match = lconfig.match(eregex);
-  const luser_match = lconfig.match(uregex);
-
-  const lemail = lemail_match ? lemail_match[1] : undefined;
-  const luser = luser_match ? luser_match[1] : undefined;
-
-  return {
-    global: {
+    result.global = {
       email: gemail,
       user: guser
-    },
-    local: {
+    }
+  }
+
+  if (!skip_local) {
+    const lconfig = readFileSync(local_path, 'utf-8');
+    const lemail_match = lconfig.match(eregex);
+    const luser_match = lconfig.match(uregex);
+    const lemail = lemail_match ? lemail_match[1] : undefined;
+    const luser = luser_match ? luser_match[1] : undefined;
+
+    result.local = {
       email: lemail,
       user: luser
     }
-  };
+  }
+
+  return result;
 }
