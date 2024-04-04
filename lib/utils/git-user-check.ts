7e4dc18e9de8b5374816452297cec296e5d84ba1
Eric Vandenberg <contact@ericvandenberg.com>
2024-04-04 02:45:55 -0600

    fix: extract git user name & email with regex

--- lib/utils/git-user-check.ts
+++ lib/utils/git-user-check.ts
@@ -1,20 +1,27 @@
 import os from 'node:os';
-import { text } from 'node:stream/consumers';
-import { accessSync, readFileSync, constants, unlink, unlinkSync } from 'node:fs'
-import { spawnSync } from 'node:child_process';
+import { accessSync, readFileSync, constants } from 'node:fs'
 
 export const git_user_check = async (path: string) => {
   const home = os.homedir();
   accessSync(`${home}/.gitconfig`, constants.R_OK | constants.W_OK);
 
-  const gconfig = readFileSync(`${home}/.gitconfig`, 'utf-8').toString().replace(/\r\n/g, '\n').replace(/\n|\t+/g, '').split('=');
-  const lconfig = readFileSync(`${path}/.git/config`, 'utf-8').toString().replace(/\r\n/g, '\n').replace(/\n|\t+/g, '').split('[user]');
+  const eregex = /email = (.*?)$/sm
+  const uregex = /name = (.*?)$/sm
 
-  const guser = gconfig[2].trim();
-  const gemail = gconfig[1] ? gconfig[1].replace('name', '').trim() : undefined;
+  const gconfig = readFileSync(`${home}/.gitconfig`, 'utf-8');
+  const lconfig = readFileSync(`${path}/.git/config`, 'utf-8');
 
-  const luser = lconfig[1] ? lconfig[1].split('=')[2].trim() : undefined;
-  const lemail = lconfig[1] ? lconfig[1].split('=')[1].replace('name', '').trim() : undefined;
+  const gemail_match = gconfig.match(eregex);
+  const guser_match = gconfig.match(uregex);
+
+  const gemail = gemail_match ? gemail_match[1] : undefined;
+  const guser = guser_match ? guser_match[1] : undefined;
+
+  const lemail_match = lconfig.match(eregex);
+  const luser_match = lconfig.match(uregex);
+
+  const lemail = lemail_match ? lemail_match[1] : undefined;
+  const luser = luser_match ? luser_match[1] : undefined;
 
   return {
     global: {
