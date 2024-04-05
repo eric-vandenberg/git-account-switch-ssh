bdc6d2cf4048ca59a17e32957bf90d634f8d3f84
Eric Vandenberg <contact@ericvandenberg.com>
2024-04-05 16:59:56 -0600

    fix: clean up ssh keys check

--- lib/utils/ssh-keys-check.ts
+++ lib/utils/ssh-keys-check.ts
@@ -2,17 +2,19 @@ import os from 'node:os';
 import { accessSync, readdirSync, constants } from 'node:fs'
 
 export const ssh_keys_check = async (): Promise<string[]> => {
-  const home = os.homedir();
-  const regex = /^(id_ed25519|id_ecdsa|id_rsa|id_dsa).+$/;
-  const keys = [];
+  try {
+    const home = os.homedir();
+    const regex = /^(id_ed25519|id_ecdsa|id_rsa|id_dsa).+$/; // id_ed25519-sk & id_ecdsa-sk will be recongnized
+    const keys = [];
 
-  accessSync(`${home}/.ssh/config`, constants.R_OK | constants.W_OK);
-
-  for (const file of readdirSync(`${home}/.ssh`)) {
-    if (regex.test(file)) {
-      keys.push(file);
+    for (const file of readdirSync(`${home}/.ssh`)) {
+      if (regex.test(file)) {
+        keys.push(file);
+      }
     }
-  }
 
-  return keys;
+    return keys;
+  } catch (err: unknown) {
+    return [];
+  }
 }
