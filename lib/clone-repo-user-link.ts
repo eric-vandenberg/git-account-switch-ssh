99a1be6e98698019d0d78c36f41788654ff1ed18
Eric Vandenberg <contact@ericvandenberg.com>
2024-04-10 00:48:35 -0600

    feat: clone a repo with selected ssh user & link for new commits

---
+++ lib/clone-repo-user-link.ts
@@ -0,0 +1,28 @@
+import { execSync } from 'node:child_process';
+import { note } from '@clack/prompts';
+
+import { ssh_config_check } from './utils/ssh-config-check';
+import { git_config_set } from './utils/git-config-set.js';
+
+export const clone_repo_user_link = async (repository: string, project: string, username: string) => {
+  try {
+    const { accounts } = await ssh_config_check();
+    const entry = accounts.find((account: Record<string, string | string[]> | undefined) => account?.User === username);
+
+    if (!!entry && entry.IdentityFile?.[0]) {
+      execSync(`GIT_SSH_COMMAND="ssh -o IdentitiesOnly=yes -i ${entry.IdentityFile[0]} -F /dev/null" git clone ${repository}`, { stdio: [] });;
+
+      await git_config_set(username, project);
+
+      note(`
+      Repo cloned!
+      \n
+      cd ${project}
+      \n
+      You are setup to commit as user ${username}
+    `, 'Success');
+    }
+  } catch (err: unknown) {
+    //
+  }
+}
