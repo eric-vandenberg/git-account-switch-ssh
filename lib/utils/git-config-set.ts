1e8fe9839cd540559223bbf8abaf3d691412f286
Eric Vandenberg <contact@ericvandenberg.com>
2024-04-11 19:57:39 -0600

    fix: bug in changing dir

--- lib/utils/git-config-set.ts
+++ lib/utils/git-config-set.ts
@@ -2,22 +2,23 @@ import { execSync } from 'node:child_process';
 
 import { gas_cache_check } from './gas-cache-check.js';
 import { ssh_config_check } from './ssh-config-check.js';
+import { IEntry } from '../types/entry.js';
 
 export const git_config_set = async (username: string, project: string = '.') => {
   try {
     const cache = await gas_cache_check();
-    const { accounts } = await ssh_config_check();
+    const accounts = await ssh_config_check();
 
     const record = cache.find((entry: { username: string; name: string; email: string; }) => entry.username === username);
-    const section = accounts.find((account: Record<string, string | string[]> | undefined) => account?.User === username);
+    const section = accounts.find((account: IEntry) => account?.User === username);
 
     if (record) {
-      execSync(`cd ${project} && git config user.name "${record.name}" && cd ..`, { stdio: [] });
-      execSync(`cd ${project} && git config user.email "${record.email}" && cd ..`, { stdio: [] });
+      execSync(`cd ${project} && git config user.name "${record.name}"`, { stdio: [] });
+      execSync(`cd ${project} && git config user.email "${record.email}"`, { stdio: [] });
     }
 
-    if (section) {
-      execSync(`cd ${project} && git config core.sshCommand "ssh -o IdentitiesOnly=yes -i ${section.IdentityFile[0]} -F /dev/null"  && cd ..`);
+    if (section?.IdentityFile?.[0]) {
+      execSync(`cd ${project} && git config core.sshCommand "ssh -o IdentitiesOnly=yes -i ${section.IdentityFile[0]} -F /dev/null"`);
     }
   } catch (err: unknown) {
 
