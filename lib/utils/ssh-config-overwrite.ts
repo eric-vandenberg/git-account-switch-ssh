14a408e681f8ac21efc6639c5b9a9771a7c20789
Eric Vandenberg <contact@ericvandenberg.com>
2024-04-11 19:56:46 -0600

    feat: add ssh config overwrite util

---
+++ lib/utils/ssh-config-overwrite.ts
@@ -0,0 +1,30 @@
+import os from 'node:os';
+import { writeFileSync } from 'node:fs';
+import { execSync } from 'node:child_process';
+import SSHConfig from 'ssh-config';
+
+import { IEntry } from '../types/entry.js';
+
+export const ssh_config_overwrite = async (users: IEntry[], addl?: Record<string, string | string[]>) => {
+  try {
+    const home = os.homedir();
+
+    const config = new SSHConfig();
+
+    users.forEach((u: IEntry) => {
+      config.append({
+        ...u,
+      });
+    })
+
+    if (!!addl && addl.IdentityFile?.[0]) {
+      config.append(addl);
+
+      execSync(`ssh-add ${addl.IdentityFile?.[0]}`);
+    }
+
+    writeFileSync(`${home}/.ssh/config`, SSHConfig.stringify(config), { encoding: 'utf-8' });
+  } catch (err: unknown) {
+    // 
+  }
+}
