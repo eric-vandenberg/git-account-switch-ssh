1226fa6f8657a7e05292b0ab6fa1bf5ce98335f5
Eric Vandenberg <contact@ericvandenberg.com>
2024-04-13 19:57:54 -0600

    chore: return restored ssh config to print

--- lib/utils/ssh-config-restore.ts
+++ lib/utils/ssh-config-restore.ts
@@ -1,11 +1,16 @@
 import os from 'node:os';
 import { execSync } from 'node:child_process';
+import { readFileSync } from 'node:fs';
 
-export const ssh_config_restore = async () => {
+export const ssh_config_restore = async (): Promise<string | undefined> => {
   try {
     const home = os.homedir();
 
     execSync(`mv ${home}/.ssh/config_backup ${home}/.ssh/config`);
+
+    const restored_config = readFileSync(`${home}/.ssh/config`, { encoding: 'utf-8' });
+
+    return restored_config;
   } catch (err: unknown) {
     //
   }
