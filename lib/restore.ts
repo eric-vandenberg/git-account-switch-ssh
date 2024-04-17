77ad169a5bccb6eb8f1420556bef0698751bb409
Eric Vandenberg <contact@ericvandenberg.com>
2024-04-16 19:32:56 -0600

    refactor: restore to original settings

---
+++ lib/restore.ts
@@ -0,0 +1,20 @@
+import { log } from '@clack/prompts';
+
+import { IEntry } from './types/entry.js';
+import { ssh_keys_delete } from './utils/ssh-keys-delete.js';
+import { ssh_config_restore } from './utils/ssh-config-restore.js';
+import { gas_cache_delete } from './utils/gas-cache-delete.js';
+
+export const restore = async (users: IEntry[]) => {
+  try {
+    const deleted_keys = await ssh_keys_delete(users);
+    const restored_config = await ssh_config_restore();
+    await gas_cache_delete();
+
+    log.step(`Removing keys:\n\n${deleted_keys.join('\n')}`);
+
+    log.step(`Restoring ssh config:\n\n${restored_config}`);
+  } catch (err: unknown) {
+    //
+  }
+}
